a53a9ebcad3b8dddadf189a8a0129874ef19515f
michiya-59 <nishino.michiya0509@gmail.com>
2023-03-23 21:45:35 +0900

    問い合わせページの実装完了

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     autoplaySpeed: 0, //自動再生のスライド切り替えまでの時間を設定
     speed: 5000, //スライドが流れる速度を設定
     cssEase: "linear", //スライドの流れ方を等速に設定
-    slidesToShow: 6, //表示するスライドの数
+    slidesToShow: 7, //表示するスライドの数
     swipe: false, // 操作による切り替えはさせない
     arrows: false, //矢印非表示
     pauseOnFocus: false, //スライダーをフォーカスした時にスライドを停止させるか
